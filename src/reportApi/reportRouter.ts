6ba0ce97b521adf9761541088e446ee133991dc3
mgillam <mic@secureideas.com>
2022-12-22 08:05:00 -0500

    Content-Type handling for csp-reports

--- src/reportApi/reportRouter.ts
+++ src/reportApi/reportRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import IDataAdapter from "../types/data/IDataAdapter";
 import { CSPViolationReportJson } from "../types/report";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 export default class ReportRouter {
   router = express.Router();
@@ -11,7 +12,7 @@ export default class ReportRouter {
     this.dataAdapater = dataAdapter;
     this.router.use(express.json());
     this.router.use(cors());
-    this.router.post("/", async (req, res) => {
+    this.router.post("/", bodyParser.json({ type: ["application/csp-report", "*/json"] }), async (req, res) => {
       const violationReport: CSPViolationReportJson = req.body;
       dataAdapter.writeViolationReport(violationReport["csp-report"]);
       res.status(204).send();
